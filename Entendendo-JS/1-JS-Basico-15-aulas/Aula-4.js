30c70fc2f64f0aca2aa4ff2747c5b1a7447721a6
Júnior Baum <juniorsbaum@hotmail.com>
2022-09-15 19:35:30 -0300

    add: aula 4

---
+++ Entendendo-JS/1-JS-Basico-15-aulas/Aula-4.js
@@ -0,0 +1,14 @@
+//Primeiros comandos
+
+//Desafio 4:
+
+//Incremente a nossa calculadora para receber os valores a partir dos dados digitados pelo usuario e mostrar como um alerta.
+
+
+
+var x = prompt("Digite o valor de x: ");
+var y = prompt("Digite o valor de y: ");
+var sum = Number(x) + Number(y);
+
+alert("a soma foi: " + sum)
+
